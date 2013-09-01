f8daf9184af4eddef107266db8b7e2a14a3eecd3
alyssaq <alyssaquek@gmail.com>
2013-09-01 21:37:39 +0800

    Base files for triggerio with parse, mustache and bootstrap3

---
+++ src/js/main.js
@@ -0,0 +1,131 @@
+
+forge.logging.info("Added js/main.js!");
+// Constants used for configuration
+var config = {
+  parseAppId: 'wmPoE4R7d53hyURxAF70Xt2EFj0IJGu1gv2pixtt',
+  parseRestKey: 'OmQTFXOOAmCFPMiCtF2oMetMLZQSgPfggGbD30AT',
+  streamName: 'alyssa'
+};
+
+// Step 1: Capture an image
+var capturePhoto = function() {
+  forge.file.getImage({width: 500, height: 500}, function (file) {
+    forge.file.URL(file, 
+      function (url) {
+        $('#photo-container').prepend($('<img>').attr('src', url));
+      }, function(err) {
+        forge.logging.error("Could not get image file: " +err);
+      });
+    uploadPhotoFile(file);
+  });
+};
+
+// Step 2: Upload the image file to Parse
+var uploadPhotoFile = function(file) {
+  forge.request.ajax({
+    url: 'https://api.parse.com/1/files/' + (new Date()).getTime() + '.jpg',
+    headers: {
+      'X-Parse-Application-Id': config.parseAppId,
+      'X-Parse-REST-API-Key': config.parseRestKey
+    },
+    type: 'POST',
+    files: [file],
+    fileUploadMethod: 'raw',
+    dataType: 'json',
+
+    success: function (data) {
+      uploadPhotoMetadata(data);
+    },
+    error: function () {
+      forge.logging.error("Problem uploading the photo");
+
+      alert('Problem uploading the photo');
+    }
+  });
+};
+
+// Step 3: Upload image metadata to Parse
+var uploadPhotoMetadata = function(data) {
+  forge.request.ajax({
+    url: 'https://api.parse.com/1/classes/Photo',
+    headers: {
+      'X-Parse-Application-Id': config.parseAppId,
+      'X-Parse-REST-API-Key': config.parseRestKey
+    },
+    type: 'POST',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({
+      file: {
+        '__type': 'File',
+        name: data.name
+      },
+      stream: config.streamName
+    }),
+    success: function (file) {
+      // Upload complete - do nothing
+    },
+    error: function () {
+      forge.logging.error("Problem uploading the metadata");
+      alert('Problem uploading the metadata');
+    }
+  });
+};
+
+// Get images from Parse
+var getPhotos = function() {
+  forge.request.ajax({
+    url: 'https://api.parse.com/1/classes/Photo',
+    headers: {
+      'X-Parse-Application-Id': config.parseAppId,
+      'X-Parse-REST-API-Key': config.parseRestKey
+    },
+    type: 'GET',
+    dataType: 'json',
+    data: {
+      'where': '{"stream": "' + config.streamName + '"}',
+      'order': '-createdAt'
+    },
+    success: function (data) {
+      $('#photo-container').children().remove();
+      data.results.forEach(function (photo) {
+        $('#photo-container').append($('<img>').attr('src', photo.file.url));
+      })
+    },
+    error: function () {
+      alert('Problem reading photos');
+    }
+  });
+};
+
+
+$(document).ready(function() {
+  // Setup 'sensible' click/touch handling
+  var clickEvent = 'ontouchend' in document.documentElement ? 'tap' : 'click';
+  if (clickEvent == 'tap') {
+    forge.logging.info("clickEvent is tap");
+    var currentTap = true;
+    $('*').on('touchstart', function (e) {
+      currentTap = true;
+      e.stopPropagation();
+    });
+    $('*').on('touchmove', function (e) {
+      currentTap = false;
+    });
+    $('*').on('touchend', function (e) {
+      if (currentTap) {
+        $(e.currentTarget).trigger('tap');
+      }
+      e.stopPropagation();
+    });
+  } else {
+    forge.logging.info("clickEvent is: " + clickEvent);
+  }
+
+
+  $('#upload-photo').bind(clickEvent, capturePhoto);
+  getPhotos();
+  forge.event.messagePushed.addListener(function (msg) {
+    alert("AQ Parse says: " + msg.alert);
+  });
+});
---
+++ src/js/weather.js
@@ -0,0 +1,67 @@
+var weather = {
+  "current_observation": {
+    "display_location": {
+      "full": "San Francisco, CA"
+    },
+    "observation_time":"Last Updated on September 20, 3:50 AM PDT",
+    "weather": "Mostly Cloudy",
+    "temp_f": 54.4,
+    "temp_c": 12.4,   
+    "relative_humidity":"89%",
+    "wind_string":"From the WNW at 4.0 MPH",
+    "icon_url":"http://icons-ak.wxug.com/i/c/k/nt_mostlycloudy.gif"
+  },
+  "forecast": {
+    "simpleforecast": {
+      "forecastday": [
+        { "date": { "weekday_short": "Thu" },
+          "period": 1,
+          "high": { "fahrenheit": "64", "celsius": "18" },
+          "low": { "fahrenheit": "54", "celsius": "12" },
+          "conditions": "Partly Cloudy",
+          "icon_url":"http://icons-ak.wxug.com/i/c/k/partlycloudy.gif" },
+        { "date": { "weekday_short": "Fri" },
+          "period": 2,
+          "high": { "fahrenheit": "70", "celsius": "21" },
+          "low": { "fahrenheit": "54", "celsius": "12" },
+          "conditions": "Mostly Cloudy",
+          "icon_url":"http://icons-ak.wxug.com/i/c/k/mostlycloudy.gif" },
+        { "date": { "weekday_short": "Sat" },
+          "period": 3,
+          "high": { "fahrenheit": "70", "celsius": "21" },
+          "low": { "fahrenheit": "52", "celsius": "11" },
+          "conditions": "Partly Cloudy",
+          "icon_url":"http://icons-ak.wxug.com/i/c/k/partlycloudy.gif" }
+      ]
+    }
+  }
+};
+
+
+function populateWeatherConditions (weather) {
+  var tmpl, output;
+
+  forge.logging.log("[populateWeatherConditions] beginning populating weather conditions");
+
+  tmpl = $("#forecast_information_tmpl").html();
+  output = Mustache.to_html(tmpl, weather.current_observation);
+  $("#forecast_information").append(output);
+  forge.logging.log("[populateWeatherConditions] finished populating forecast information");
+
+  tmpl = $("#current_conditions_tmpl").html();
+  output = Mustache.to_html(tmpl, weather.current_observation);
+  $("#current_conditions").append(output);
+  forge.logging.log("[populateWeatherConditions] finished populating current conditions");
+
+  tmpl = $("#forecast_conditions_tmpl").html();
+  output = Mustache.to_html(tmpl, weather.forecast.simpleforecast);
+  $("#forecast_conditions table tr").append(output);
+  forge.logging.log("[populateWeatherConditions] finished populating forecast conditions");
+
+  forge.logging.log("[populateWeatherConditions] finished populating weather conditions");
+};
+
+
+$(function () {
+  populateWeatherConditions(weather);
+});
